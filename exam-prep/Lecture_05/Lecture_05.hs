d8835e4f118a4048641185e26af25deb082becdf
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2024-01-03 09:28:04 +0100

    Exam prep: Lecture 05

--- exam-prep/Lecture_05/Lecture_05.hs
+++ exam-prep/Lecture_05/Lecture_05.hs
@@ -50,3 +50,36 @@ wrapup'(x:xs) =
     where 
         fst:rst = wrapup' xs
         (h:t) = fst
+
+{-5-}
+triples:: Num a => [(a,a,a)] -> ([a],[a],[a])
+triples [] = ([],[],[])
+triples [(a,b,c)] = ([a],[b],[c])
+triples ((x,y,z):xs) = (x:xs', y:ys', z:zs')
+    where (xs', ys', zs') = triples xs
+
+{-a-}
+--run-length encoding
+rle :: Eq a => [a] -> [(a, Int)]
+rle [] = []
+rle (x:xs) = rle' (x, 1) xs
+    where
+        rle' (x, n) [] = [(x, n)]
+        rle' (x, n) (y:ys) =
+            if x == y
+                then rle' (x, n+1) ys
+                else (x, n) : rle' (y, 1) ys
+{-b-}
+amy:: Integral a => [a] -> Bool
+amy [] = False
+amy (x:xs) = odd x || amy xs
+
+amy':: Integral a => [a] -> Bool
+amy' = any odd
+
+{-c-}
+frequencies :: String -> [(Char, Int)]
+frequencies [] = []
+--x er det første element i listen. Det andet element i tuplen er hvor gange x optræder og det tæller vi ved at lave en ny liste med kun det element og tage længden af det.
+-- Vi kalder igen frequencies og fjerner x fra listen
+frequencies (x:xs) = (x, length (x : filter (== x) xs)) : frequencies (filter (/= x) xs)
