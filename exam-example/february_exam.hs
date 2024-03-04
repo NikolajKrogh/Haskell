4635a7cb9971cb348ec3d5d404137ed87f5320b7
Nikolaj Kofod Krogh <nkrogh20@student.aau.dk>
2024-03-04 22:16:44 +0100

    february exam

--- exam-example/february_exam.hs
+++ exam-example/february_exam.hs
@@ -43,3 +43,61 @@ dwindle' xs = [drop i xs | i <- [0 .. length xs]]
 -- 3.3
 dwindle'' :: [a] -> [[a]]
 dwindle'' xs = foldr (\x acc -> (x : head acc) : acc) [[]] xs
+
+-- PROBLEM 4
+
+data Status a = Fresh a | Used a deriving Show
+instance Functor Status where
+  fmap f (Fresh x) = Fresh (f x)
+  fmap f (Used x) = Used (f x)
+
+-- 4.1
+instance Applicative Status where
+  pure = Fresh
+  --(<*>) :: Status (a -> b) -> Status a -> Status b
+  (Fresh f) <*> (Fresh x) = Fresh (f x)
+  (Fresh f) <*> (Used x) = Used (f x)
+  (Used f) <*> (Used x) = Used (f x)
+  (Used f) <*> (Fresh x) = Used (f x)
+
+  -- 4.2
+instance Monad Status where
+  return = pure
+  -- (>>=) :: Status a -> (a -> Status b) -> Status b
+  Fresh x >>= f = f x
+  Used x >>= f = f x
+
+-- 4.3
+minimize :: Ord b => Status b -> Status b -> Status b
+minimize x y = do
+  a <- x
+  b <- y
+  return(min a b)
+
+-- PROBLEM 5
+
+-- 5.1
+--func1:: (Num a) => Bool -> a -> (a, a)
+func1 True x = (x+1, x+2)
+
+-- 5.2
+--func2:: a1 -> a2 -> (a2, a2) -> (a1, [a2])
+func2 x a (b,c) = (x,[a,b,c])
+
+--func3:: Num a => Maybe [a -> a]
+func3 = Just [\x -> x+x] -- Correct if you ask the compiler with :t func3 = Just [\x -> x+x]
+
+--func4:: p1 -> p2 -> [a] 
+func4 x y = []
+
+-- PROBLEM 6
+
+-- 6.1 
+squares [] = []
+squares (x:xs) = x*x : squares xs 
+
+-- 6.2
+squares' = map (\x -> x*x) [1..]
+
+-- 6.3
+squares'' = zipWith (*) [1..] [1..]
